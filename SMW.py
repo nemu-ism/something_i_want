da434297d55c6d1833691550f23f2f0d8345035c
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-01-08 00:52:07 +0900

    Ranking -> Search

--- SMW.py
+++ SMW.py
@@ -6,47 +6,50 @@ import urllib.request
 import json
 
 def yapi_topics():
-    url = 'http://shopping.yahooapis.jp/ShoppingWebService/V1/json/categoryRanking?'
-    appid = 'dj00aiZpPU1YWU1IRXYwaDhqeCZzPWNvbnN1bWVyc2VjcmV0Jng9MDY-'
+    url = 'https://shopping.yahooapis.jp/ShoppingWebService/V1/json/itemSearch?'
+    id = 'dj00aiZpPU1YWU1IRXYwaDhqeCZzPWNvbnN1bWVyc2VjcmV0Jng9MDY-'
     params = urllib.parse.urlencode(
-        {'appid': appid,
-         'offset':1,
-         'period':'weekly',
-         'generation':30,
-         'gender':'female',})
+        {'appid': id,
+         'query':'パソコン',
+         'price_to':50000,
+         'hits':20,
+         'availability':1})
 
-    #print url + params
     response = urllib.request.urlopen(url + params)
     return response.read()
 
 def do_json(s):
     data = json.loads(s)
-    #print(json.dumps(data, sort_keys=True, indent=4)); sys.exit()
+    print(json.dumps(data, ensure_ascii=False, sort_keys=False, indent=4)); sys.exit()
 
     #jsonの階層の"Result"以下を辞書にする。keyは番号：その次の配列がvalueになっている
     item_list = data["ResultSet"]["0"]["Result"]
 
     #空のディクショナリを作る
-    ranking = {}
-    for  k, v in item_list.items():
+    results = {}
+    for k, v in item_list.items():
         try:
-            rank = int(v["_attributes"]["rank"])
-            vector = v["_attributes"]["vector"]
-            name  = v["Name"]
-            ranking[rank] = [vector, name]
+            number = int(v["_attributes"]["index"])
+            name = v["Headline"]
+            url = v["Url"]
+            condition = v["Condition"]
+            price = v["Price"]["_value"]
+            rate_average = v["Review"]["Rate"]
+            rate_count = v["Review"]["Count"]
+            results[number] = [name, condition, price, rate_average, rate_count, url]
         except:
-            if k == "RankingInfo":
-                StartDate = v["StartDate"]
-                EndDate = v["EndDate"]
+            if k == "Request":
+                query = v["Query"]
 
     print('-' * 40)
-    print(u"集計開始日:", StartDate)
-    print(u"集計終了日:", EndDate)
+    print("検索ワード："+ query)
     print('-' * 40)
-    ranking_keys = list(ranking.keys())
-    ranking_keys.sort()
-    for i in ranking_keys:
-        print(i, ranking[i][0], ranking [i][1])
+    results_keys = list(results.keys())
+    results_keys.sort()
+    for i in results_keys:
+        print(i, results[i][0])
+        print(' ' *6, results[i][1], '{:,}円(税込)'.format(int(results[i][2])), '平均評価{0:}点({1:,}人中)'.format(results[i][3], int(results[i][4])))
+        print(' ' *6, '商品ページ：'+ results[i][5])
 
 if __name__ == '__main__':
     json_str = yapi_topics()
