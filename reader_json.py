c737428190a78aa634058a7d16d894f684c1b542
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-01-09 09:27:44 +0900

    separate reader_json

---
+++ reader_json.py
@@ -0,0 +1,46 @@
+import generator_url
+
+def print_list(data, args):
+    hits_total = int(data["ResultSet"]["totalResultsAvailable"])
+    hits_offset = int(data["ResultSet"]["firstResultPosition"])
+    item_list = data["ResultSet"]["0"]["Result"]
+    results = {}
+    for k, v in item_list.items():
+        try:
+            number = int(v["_attributes"]["index"])
+            name = v["Name"]
+            url = v["Url"]
+            condition = v["Condition"]
+            price = int(v["Price"]["_value"])
+            rate_average = v["Review"]["Rate"]
+            rate_count = int(v["Review"]["Count"])
+            results[number] = [name, condition, price, rate_average, rate_count, url]
+        except:
+            if k == "Request":
+                query = v["Query"]
+
+    # ifs are dirty :(
+    dic = generator_url.generator_dic(args[1:])
+    print('-' * 40)
+    if 'sort' in dic:
+        print(dic['sort'], end='')
+    print('検索ワード：', query)
+    if 'condition' in dic:
+        print('状態:{:} only'.format(dic['condition']))
+    if 'price_from' or 'price_to' in dic:
+        print('価格：', end='')
+        if 'price_from' in dic:
+            print('{:,}円から'.format(int(dic['price_from'])), end='')
+        if 'price_to' in dic:
+            print('{:,}円まで'.format(int(dic['price_to'])), end='')
+        print('')
+    print('{0:,}件中　{1:,}～{2:,}件'.format(hits_total, hits_offset, hits_offset+9))
+    print('-' * 40)
+
+    results_keys = list(results.keys())
+    results_keys.sort()
+    for i in results_keys:
+        print(i, results[i][0])
+        print(' ' *6, results[i][1], '{:,}円(税込)'.format(results[i][2]),
+              '平均評価{0:}点({1:,}人中)'.format(results[i][3], results[i][4]))
+        print(' ' *6, '商品ページ：', results[i][5])
