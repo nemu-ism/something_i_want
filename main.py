c65b9c5bf5ee0ce94c8f69eecff17394bff53296
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2018-01-09 11:07:15 +0900

    メイン処理をmain.pyに移行

---
+++ main.py
@@ -0,0 +1,14 @@
+import generator_url
+import reader_json
+
+class Main:
+    def __init__(self):
+        pass
+
+    generator = generator_url.UrlGenerator()
+    reader = reader_json.JsonReader()
+
+    def main(self, *args):
+        requests = args[1:]
+        json_generated = self.generator.generate_json(requests)
+        self.reader.print_list(json_generated, args)
--- siw.py
+++ siw.py
@@ -1,18 +1,18 @@
 import sys
+import main
 import generator_url
 import reader_json
 
 # sys.argv = terminal入力
 args = sys.argv
+main = main.Main()
 generator = generator_url.UrlGenerator()
 reader = reader_json.JsonReader()
 
 # main
 if __name__ == '__main__':
     try:
-        requests = args[1:]
-        json_generated = generator.generate_json(requests)
-        reader.print_list(json_generated, args)
+        main.main(args)
     except:
         print("実行を終了します。")
         sys.exit()
